0769ba7be4aa18561fb350e7c24e1791cfeda3cc
Neon <neon@Neon.local>
2015-02-08 21:29:49 +0100

    Initial Commit

---
+++ MTURLImageCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTURLImageCache
+//
+//  Created by Neon on 08/02/15.
+//  Copyright (c) 2015 MineTilbud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MTURLImageCache/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  MTURLImageCache
+//
+//  Created by Neon on 08/02/15.
+//  Copyright (c) 2015 MineTilbud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
